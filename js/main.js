f74d53515783a23b21bced32d7a103f8001e69b9
Sam Tyrie <tyri0004@algonquinlive.com>
2026-09-17 11:46:38 -0400

    changes made

--- js/main.js
+++ js/main.js
@@ -6,3 +6,23 @@ let myName = "Sam Tyrie";
 console.log(myName);
 
 const myAddress ="Earth";
+
+console.log(myAddress);
+
+console.log("My name is" + myName + "I live on" + myAddress);
+
+const numA = 10; 
+const numB = 5; 
+
+console.log(numA + numB);
+console.log("the sum of numA and numB is" + (numA + numB));
+
+const numC = numA + numB; 
+console.log(`the sum of numA and numB is ${numC}`);
+
+let petOwner = true;
+console.log("Is "+myName+" a pet owner?"+petOwner);
+
+console.log(`is number a greater than number b? ${numA < numB}`);
+
+console.log(numB > numC || numA > numB)
